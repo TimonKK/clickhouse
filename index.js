711791c3095ed31d2f68346be75806e33e457dcb
Vadim Baryshev <vadimbaryshev@gmail.com>
2017-06-19 17:00:21 +0300

    Support for NULL values

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ class ClickHouse {
 			{
 				url     : this.getHost() + '?query=' + url,
 				body    : values.map(function(row) {
-					return row.map(function(column) { return jsesc(column); }).join('\t');
+					return row.map(function(column) { return (typeof column === 'undefined' || column === null) ? '\\N' : jsesc(column); }).join('\t');
 				}).join('\n'),
 				headers : {
 					'Content-Type': 'text/plain'
