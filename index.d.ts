1c1015941d92c07a5604962e7dd9beae74c289ba
Lypko Oleksandr <aleksandr.lypko@piano.io>
2019-09-27 10:19:14 +0300

    Added return-types.

--- index.d.ts
+++ index.d.ts
@@ -13,13 +13,13 @@ declare module 'clickhouse' {
   }
 
   export class WriteStream extends Stream.Transform {
-    writeRow(data: Array<any>);
-    exec();
+    writeRow(data: Array<any>): void;
+    exec(): Promise<{}>;
   }
 
   class QueryCursor {
     toPromise(): Promise<Object[]>;
-    exec(callback: callbackExec);
+    exec(callback: callbackExec): void;
     stream(): Stream | WriteStream;
   }
 }
